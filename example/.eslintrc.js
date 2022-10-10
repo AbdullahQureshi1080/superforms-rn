59d3b5a7f3c15d42dfd3b80b4b47b319f137e095
Abdullah Qureshi <adnx1080@gmail.com>
2022-10-10 21:28:01 +0500

    chore(examples/e2e & tests setup): setting env for e2e tests with detox and jest - TDD

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -3,10 +3,15 @@ module.exports = {
   extends: ['@react-native-community/eslint-config', 'eslint-config-prettier'],
   // extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'detox'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.e2e.js'],
+      env: {
+        'detox/detox': true,
+        jest: true,
+        'jest/globals': true,
+      },
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
