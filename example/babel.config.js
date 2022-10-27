975e1f9db7d14e72b9e54a675373404f3a6d1fb3
Abdullah Qureshi <adnx1080@gmail.com>
2022-10-27 22:16:12 +0500

    fix(/example package.json jest.config.ts): fixing the dev-dependency issue,adding example project
    Resolving the dev-dependency issues, accidently added dependencies to dev-dependecnies, resolved
    this, ignored dist tests added module ignore in jest-config, added a new example project
    awesomeTSProject to mock superform usage. Removed old example as it had build issues

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,21 +1,3 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    // [
-    //   '@babel/preset-env',
-    //   {targets: {node: 'current'}, modules: 'commonjs', loose: true},
-    // ],
-    // '@babel/preset-typescript',
-    // '@babel/preset-react',
-  ],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@Components': './src/components/src',
-        },
-      },
-    ],
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
