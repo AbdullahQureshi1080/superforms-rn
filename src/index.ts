f46d594bb6c121ce520a9c0b19bffd0c1ce6f814
Abdullah Qureshi <adnx1080@gmail.com>
2023-01-08 17:08:44 +0500

    fix(common/datepicker): fixed the ui-state issue, updating date on change
    The issue was that when data was being fetched from api/ supplying data to form asyncronously the
    date field value was not updating UI.
    
    https://github.com/AbdullahQureshi1080/superforms-rn/issues/16

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,11 @@ export { default as ImagePicker } from "./Common/Image/ImagePicker";
 // Form Exports
 export { default as Form } from "./Form/Form";
 export { default as FormField } from "./Form/FormField/FormField";
+// Date Selector
 export { default as FormDateSelector } from "./Form/FormDateSelector/FormDateSelector";
-export { default as SubmitButton } from "./Form/SubmitButton/SubmitButton";
+export { default as FormDate } from "./Form/FormDateSelector/FormDateSelector";
+// Image Selector
 export { default as FormImageSelector } from "./Form/FormImageSelector/FormImageSelector";
+export { default as FormImage } from "./Form/FormImageSelector/FormImageSelector";
+
+export { default as SubmitButton } from "./Form/SubmitButton/SubmitButton";
